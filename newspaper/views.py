b23802b8f1447f121cfe9c4deb4319b59e1f4a46
Mykhailo Vovchok <mivvaw@gmail.com>
2023-03-29 16:15:47 +0300

    add urls to my app,connected with root url, created index(view) and base.html page and index.html

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from newspaper.models import Redactor, Article, Topic
+
+
+def index(request):
+    num_redactors = Redactor.objects.count()
+    num_articles = Article.objects.count()
+    num_topics = Topic.objects.count()
+
+    # num_visits = request.session.get("num_visits", 0)
+    # request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_articles": num_articles,
+        "num_topics": num_topics
+    }
+
+    return render(request, "newspaper/index.html", context=context)
