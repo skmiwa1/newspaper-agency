7c5f7e06961fc06df3162f45d49b23583f42fbbf
Mykhailo Vovchok <mivvaw@gmail.com>
2023-03-29 15:50:19 +0300

    add new models to models.py

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,33 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topic = models.ForeignKey(to=Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="articles")
+
+    def __str__(self):
+        return self.title
