e81bef8039387edfd14428dd8ae4e7fbed77d1c7
Mykhailo Vovchok <mivvaw@gmail.com>
2023-04-02 20:14:53 +0300

    removed insecure Dja-secret key settings

--- Newspaper_project/settings.py
+++ Newspaper_project/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-2n%=dpu0&c$=)33qk+qvkie0jtbj4s1q!ize8a$l!_%nrt)vx9')
-
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
